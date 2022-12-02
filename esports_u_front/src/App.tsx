1432a23d1aef6731afbb73cfb24451464e7e1285
YPZ-24 <aylinypz@gmail.com>
2022-12-02 16:57:48 -0600

    Front: Template

---
+++ esports_u_front/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hola...!</Button>
+    </div>
+  );
+}
+
+export default App;
